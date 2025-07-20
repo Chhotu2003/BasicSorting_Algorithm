ae4ab42e97b2bc2e7ca4cefffab26d789e18c48b
Chhotu kumar <168346582+Chhotu2003@users.noreply.github.com>
2025-07-20 19:26:37 +0000

    Insertion sort Aarray based in java

---
+++ InsertionSort.java
@@ -0,0 +1,22 @@
+public class InsertionSort {
+    public static void insertedSort(int[] arr) {
+        for(int i=0;i<arr.length;i++) {
+            int curr = arr[i];
+            int prev = i-1;
+            // finding out the correct pos to insert
+            while (prev >= 0 && arr[prev] > curr) {
+                arr[prev+1] = arr[prev];
+                prev--;
+            }
+            //insertion 
+            arr[prev+1] = curr;
+        }
+        for(int arr1 : arr) {
+           System.out.print(arr1 + " ");
+        }
+    }
+    public static void main(String[] args) {
+        int arr[]={5,4,1,3,2};
+        insertedSort(arr);
+    }
+}
