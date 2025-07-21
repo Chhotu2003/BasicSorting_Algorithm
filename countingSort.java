e36e20c3597aad6a01698b05846f3f9b808f6870
Chhotu kumar <168346582+Chhotu2003@users.noreply.github.com>
2025-07-21 18:24:52 +0000

    Counting sorting Array in java

---
+++ countingSort.java
@@ -0,0 +1,28 @@
+public class countingSort {
+    public static void main(String[] args) {
+        int[] arr={1,4,1,3,2,4,3,7};
+        CountSort(arr);
+    }
+    public static void CountSort(int[] arr){
+      int largest = Integer.MIN_VALUE;
+      for(int i=0;i<arr.length;i++){
+        largest = Math.max(largest, arr[i]);
+      }
+      int count[]=new int[largest+1];
+      for(int i=0;i<arr.length;i++){
+        count[arr[i]]++;
+      }
+      //sorting
+      int j=0;
+      for(int i=0;i<count.length;i++){
+        while (count[i]>0) {
+            arr[j]=i;
+            j++;
+            count[i]--;
+        }
+      }
+      for(int arr1: arr){
+        System.out.print(arr1+" ");
+      }
+    }
+}
